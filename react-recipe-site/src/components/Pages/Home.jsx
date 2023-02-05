a70e70c90b2701af37669f289f02aef67e43ac4f
dshipuk <dshipuk@yahoo.com>
2023-02-04 16:07:19 -0800

    Created Express Server running on Port provided in .env file Created Basic Server Router to test if server is runnning Created Auth Folder with auth-router.js(WIP)

--- react-recipe-site/src/components/Pages/Home.jsx
+++ react-recipe-site/src/components/Pages/Home.jsx
@@ -1,39 +1,14 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="container">
       <header>
         <NavBar />
       </header>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nam
-          illo adipisci maiores itaque iusto omnis harum impedit accusamus
-          quibusdam incidunt, iste sed vitae, natus dolorum ratione expedita
-          sint culpa! Hic odit accusantium a! Cumque quibusdam minus deserunt.
-          Accusantium odit fugiat dignissimos. Eos eaque, mollitia a rem ab
-          recusandae deserunt perspiciatis magni, necessitatibus repudiandae
-          facere veniam dolorum ipsa distinctio quia est illo laborum quas
-          nostrum, harum deleniti quasi. Odio, illum nemo quidem sit omnis
-          consequuntur modi consectetur exercitationem minima eaque enim autem
-          natus distinctio dolore aut, quisquam velit saepe. Nam perferendis
-          sapiente deserunt omnis esse ipsam, minima, fugiat veniam laudantium
-          quasi, voluptatibus illum doloribus aliquam pariatur numquam ipsa
-          quam! Incidunt blanditiis, voluptatem repellat optio repudiandae alias
-          vitae inventore nostrum beatae quisquam delectus laborum iusto ex
-          fugiat quos voluptates commodi cumque tenetur minus. Architecto
-          asperiores facere molestias a impedit voluptatum enim mollitia maiores
-          nostrum quia laudantium, harum facilis animi nemo dolorum officiis
-          natus laborum! Ducimus quaerat voluptatum perspiciatis voluptatem
-          voluptate quasi similique molestiae nesciunt dolor dolore error nam
-          modi assumenda facilis unde, fugiat eius tenetur dignissimos sunt
-          reiciendis blanditiis autem consequuntur. Vero doloribus quas minima,
-          culpa libero repellat at cupiditate facere adipisci fugiat saepe
-          excepturi voluptates deleniti incidunt quos et ipsam!
-        </p>
-      </div>
+      
     </div>
   );
 };
